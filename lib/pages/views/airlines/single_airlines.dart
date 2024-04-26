daa75d3fe4e2a614a66f92e7742ee461231625c5
abhishekkarky <abhishekkarki40@gmail.com>
2024-04-26 08:19:31 +0545

    Map

--- lib/pages/views/airlines/single_airlines.dart
+++ lib/pages/views/airlines/single_airlines.dart
@@ -1,28 +1,29 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:travel_booking/pages/model/book_model.dart';
 import 'package:travel_booking/repo/booking_repo.dart';
-import 'package:travel_booking/routes/route_generator.dart';
 
 import '../../../providers/auth_view_model.dart';
 import '../../../providers/single_hotel_viewmodel.dart';
 
 class SingleAirlinesScreen extends StatelessWidget {
   String id;
-  SingleAirlinesScreen({Key? key,required this.id}) : super(key: key);
+  SingleAirlinesScreen({super.key, required this.id});
   static const String routeName = "/single-airlines";
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<SingleHotelViewModel>(
-        create: (_) => SingleHotelViewModel(), child: SingleAirlinesBody(id: id,));
+        create: (_) => SingleHotelViewModel(),
+        child: SingleAirlinesBody(
+          id: id,
+        ));
   }
 }
 
 class SingleAirlinesBody extends StatefulWidget {
   String id;
-  SingleAirlinesBody({Key? key,required this.id}) : super(key: key);
+  SingleAirlinesBody({super.key, required this.id});
 
   @override
   State<SingleAirlinesBody> createState() => _SingleAirlinesBodyState();
@@ -36,7 +37,8 @@ class _SingleAirlinesBodyState extends State<SingleAirlinesBody> {
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _singleProductViewModel = Provider.of<SingleHotelViewModel>(context, listen: false);
+      _singleProductViewModel =
+          Provider.of<SingleHotelViewModel>(context, listen: false);
       _authViewModel = Provider.of<AuthViewModel>(context, listen: false);
 
       print("this is args ${widget.id}");
@@ -72,135 +74,166 @@ class _SingleAirlinesBodyState extends State<SingleAirlinesBody> {
   Widget build(BuildContext context) {
     return Consumer2<SingleHotelViewModel, AuthViewModel>(
         builder: (context, singleProductVM, authVm, child) {
-          return singleProductVM.airlines == null
-              ? Scaffold(
-            body: Container(
-              child: Text("Error"),
-            ),
-          )
-              : singleProductVM.airlines!.id == null
-              ? Scaffold(
-            body: Center(
-              child: Container(
-                child: Text("Please wait..."),
+      return singleProductVM.airlines == null
+          ? Scaffold(
+              body: Container(
+                child: const Text("Error"),
               ),
-            ),
-          )
-              : Scaffold(
-            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-            floatingActionButton: Container(
-                height: 70,
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  border: Border(
-                    top: BorderSide(width: 1, color: Colors.black12),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                    onPressed: () {
-                      showDialog(context: context, builder: (context) {
-                        return AlertDialog(
-                          actions: [
-                            TextButton(onPressed: (){
-                              Navigator.of(context).pop();
-                            }, child: const Text("No")),
-                            TextButton(onPressed: () {
-
-
-                              BookingRepo().addToCart(BookingModel(
-                                bookingName: singleProductVM.airlines?.airlineName ?? "",
-                                bookingType: "Airlines",
-                                imageUrl: singleProductVM.airlines!.imageUrl,
-                                price: singleProductVM.airlines!.price,
-                                productId: singleProductVM.airlines!.id,
-                              )).then((value) {
-                                Navigator.of(context).pop();
-                                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Booking successful")));
-                              });
-                            }, child: const Text("Yes"))
-                          ],
-                          title: const Text("Confirm"),
-                          content: Text("Are you sure you want to book ${singleProductVM.airlines!.airlineName}",style: TextStyle(fontSize: 20),),
-                        );
-                      },);
-                    },
-                    child: Text("Book now",style: TextStyle(color: Colors.white),),
+            )
+          : singleProductVM.airlines!.id == null
+              ? Scaffold(
+                  body: Center(
+                    child: Container(
+                      child: const Text("Please wait..."),
+                    ),
                   ),
                 )
-            ),
-            appBar: AppBar(
-            ),
-            backgroundColor: Color(0xFFf5f5f4),
-            body: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Image.network(
-                    singleProductVM.airlines!.imageUrl.toString(),
-                    height: 400,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                    errorBuilder:
-                        (BuildContext context, Object exception, StackTrace? stackTrace) {
-                      return Image.asset(
-                        'assets/images/logo.png',
-                        height: 400,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      );
-                    },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
+              : Scaffold(
+                  floatingActionButtonLocation:
+                      FloatingActionButtonLocation.centerDocked,
+                  floatingActionButton: Container(
+                      height: 70,
                       width: double.infinity,
-                      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                      decoration: BoxDecoration(color: Colors.white70),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Rs. " + singleProductVM.airlines!.price.toString(),
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.green,
-                                fontWeight: FontWeight.w900),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 5),
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        border: Border(
+                          top: BorderSide(width: 1, color: Colors.black12),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green),
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text("No")),
+                                    TextButton(
+                                        onPressed: () {
+                                          BookingRepo()
+                                              .addToCart(BookingModel(
+                                            bookingName: singleProductVM
+                                                    .airlines?.airlineName ??
+                                                "",
+                                            bookingType: "Airlines",
+                                            imageUrl: singleProductVM
+                                                .airlines!.imageUrl,
+                                            price:
+                                                singleProductVM.airlines!.price,
+                                            productId:
+                                                singleProductVM.airlines!.id,
+                                          ))
+                                              .then((value) {
+                                            Navigator.of(context).pop();
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(const SnackBar(
+                                                    content: Text(
+                                                        "Booking successful")));
+                                          });
+                                        },
+                                        child: const Text("Yes"))
+                                  ],
+                                  title: const Text("Confirm"),
+                                  content: Text(
+                                    "Are you sure you want to book ${singleProductVM.airlines!.airlineName}",
+                                    style: const TextStyle(fontSize: 20),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                          child: const Text(
+                            "Book now",
+                            style: TextStyle(color: Colors.white),
                           ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            singleProductVM.airlines!.airlineName.toString(),
-                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            "${singleProductVM.airlines!.destination} - ${singleProductVM.airlines!.arrival}",
-                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            singleProductVM.airlines!.baggage.toString(),
-                            style: TextStyle(
-                              fontSize: 22,
-                            ),
-                          ),
-                          const SizedBox(height: 100,)
-                        ],
+                        ),
                       )),
-                ],
-              ),
-            ),
-          );
-        });
+                  appBar: AppBar(),
+                  backgroundColor: const Color(0xFFf5f5f4),
+                  body: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Image.network(
+                          singleProductVM.airlines!.imageUrl.toString(),
+                          height: 400,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                          errorBuilder: (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                            return Image.asset(
+                              'assets/images/logo.png',
+                              height: 400,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            );
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 20),
+                            decoration:
+                                const BoxDecoration(color: Colors.white70),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Rs. ${singleProductVM.airlines!.price}",
+                                  style: const TextStyle(
+                                      fontSize: 30,
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.w900),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  singleProductVM.airlines!.airlineName
+                                      .toString(),
+                                  style: const TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "${singleProductVM.airlines!.destination} - ${singleProductVM.airlines!.arrival}",
+                                  style: const TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  singleProductVM.airlines!.baggage.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 100,
+                                )
+                              ],
+                            )),
+                      ],
+                    ),
+                  ),
+                );
+    });
   }
 }
